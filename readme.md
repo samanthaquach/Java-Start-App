dbd8ebca664f867c700964976db4c356e1b995d8
samq1 <samanthatquach@gmail.com>
2019-01-09 16:19:38 -0600

    Add Gradle to Java App

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Java-Start-App
+Building Application with Spring Boot
---
+++ webapp-gradle-springboot/src/main/java/hello/Greeter.java
@@ -0,0 +1,15 @@
+package main.java.hello;
+
+import org.joda.time.*;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class Greeter {
+    public String sayHello() {
+//        DateTime dt = DateTime.now(DateTimeZone.UTC);
+        LocalDateTime localDateTime = new LocalDateTime();
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy hh:mm aa");
+        String str = dtf.print(localDateTime);
+        return "Hello world! It's currently " + str;
+    }
+}
